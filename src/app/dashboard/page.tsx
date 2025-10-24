d2df25f5595af25be6871746a93d57db38b2031f
Chel0626 <chel0626@github.com>
2025-10-24 15:51:08 -0300

    remove: TimeBasedWidget from dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import { WeatherWidget } from '@/components/WeatherWidget';
-import { TimeBasedWidget } from '@/components/TimeBasedWidget';
 import { DailyRouteWidget } from '@/components/DailyRouteWidget';
 import { PaymentsDueWidget } from '@/components/PaymentsDueWidget';
 import { OverduePaymentsWidget } from '@/components/OverduePaymentsWidget';
@@ -7,8 +6,6 @@ import { OverduePaymentsWidget } from '@/components/OverduePaymentsWidget';
 export default function DashboardPage() {
   return (
     <div className="space-y-6">
-      <TimeBasedWidget />
-
       <WeatherWidget />
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
